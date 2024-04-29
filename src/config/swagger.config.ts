4e454e249f81b3f0c94d542e289f2d44dcbcc3b2
Farida Fadilah <faridafadilah@gmail.com>
2024-04-29 16:20:45 +0700

    feat: update swagger config

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,6 +1,10 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ApiTags, DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
-import { SecuritySchemeObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import {
+  OpenAPIObject,
+  OperationObject,
+  SecuritySchemeObject,
+} from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 
 export const SWAGGER_AUTH_OPTIONS: SecuritySchemeObject = {
   type: 'http',
@@ -8,8 +12,37 @@ export const SWAGGER_AUTH_OPTIONS: SecuritySchemeObject = {
   bearerFormat: 'Bearer',
 };
 
-const title = 'BPK API Module Admin Kurikulum';
-const description = `BPK API module admin kurikulum documentation`;
+const web = {
+  title: 'BPK API Module Admin Kurikulum (Web Version)',
+  description: 'BPK API module admin kurikulum documentation for web',
+};
+
+const mobile = {
+  title: 'BPK API Module Admin Kurikulum (Mobile Version)',
+  description: 'BPK API module admin kurikulum documentation for mobile',
+};
+
+export const MobileTag = 'Mobile';
+export const IsMobile = () => ApiTags(MobileTag);
+
+/**
+ * Filters out mobile routes from the OpenAPI document.
+ *
+ * @param doc - The OpenAPI document to filter.
+ * @returns The filtered OpenAPI document.
+ */
+export function filterMobileRoutes(doc: OpenAPIObject) {
+  const webDoc = structuredClone(doc);
+
+  Object.entries(webDoc.paths).map(([k, path]) => {
+    Object.entries(path).forEach(([k, operation]) => {
+      const op = operation as OperationObject;
+      if (op.tags.some((tag) => tag.startsWith('mobile'))) delete path[k];
+    });
+  });
+
+  return webDoc;
+}
 
 /**
  * Configures the Swagger module for the NestJS application.
@@ -20,22 +53,28 @@ export function swaggerConfig(
   app: INestApplication,
   apiVersion?: string,
 ): void {
-  const config = new DocumentBuilder()
-    .setTitle(title)
-    .setDescription(description)
+  const fullConfig = new DocumentBuilder()
+    .setTitle(mobile.title)
+    .setDescription(mobile.description)
     .setVersion(apiVersion)
     .addBearerAuth(SWAGGER_AUTH_OPTIONS)
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const fullDocumentation = SwaggerModule.createDocument(app, fullConfig);
 
-  /**
-   * TOOO: Add API versioning
-   * currently we not implemented this, because we need to change the API version in the code
-   * and also in the frontend need to set the version of API.
-   *
-   * SwaggerModule.setup(`api/v${apiVersion}/docs`, app, document);
-   */
+  const webConfig = new DocumentBuilder()
+    .setTitle(web.title)
+    .setDescription(web.description)
+    .setVersion(apiVersion)
+    .addBearerAuth(SWAGGER_AUTH_OPTIONS)
+    .build();
 
-  SwaggerModule.setup('api/docs', app, document);
+  const webDocumentation = SwaggerModule.createDocument(app, webConfig);
+  Object.assign(
+    webDocumentation.paths,
+    filterMobileRoutes(fullDocumentation).paths,
+  );
+
+  SwaggerModule.setup(`api/docs/v${apiVersion}/web`, app, webDocumentation);
+  SwaggerModule.setup(`api/docs/v${apiVersion}/mobile`, app, fullDocumentation);
 }
